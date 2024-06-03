191b312487aff9cc17a1dc9105ebbd396efcc350
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-06-03 16:33:08 -0300

    1h58min

--- app/controllers/inn_dashboard/sales_controller.rb
+++ app/controllers/inn_dashboard/sales_controller.rb
@@ -1,6 +1,30 @@
 class InnDashboard::SalesController < InnDashboard::InnDashboardController
   def new
-    @sale = Sale.new
+    @sale = @inn.sales.build
     @inn_rooms = @inn.inn_rooms
+  end
+
+  def create
+    @sale = @inn.sales.build(sale_params)
+    @inn_rooms = @inn.inn_rooms
+
+    if @sale.save
+      return redirect_to inn_management_path, notice: 'Promoção cadastrada com sucesso'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def sale_params
+    params.require(:sale)
+      .permit(
+        :name,
+        :start_date, 
+        :end_date,
+        :inn_room_id,
+        :discount_percentage
+      )
   end
 end
--- spec/system/inn_owner/inn_owner_register_sale_spec.rb
+++ spec/system/inn_owner/inn_owner_register_sale_spec.rb
@@ -23,4 +23,35 @@ describe 'Inn owner register sale' do
     expect(page).to have_field 'Quarto'
     expect(page).to have_field 'Valor do desconto (em %)'
   end
+
+  it 'successfully' do
+    inn_owner = InnOwner.create!(first_name: 'Joao', last_name: 'Almeida', 
+    document: '53783222001', email: 'joao@email.com', password: '123456')
+
+    inn = inn_owner.create_inn!(name: 'Pousada do Almeidinha', registration_number: '30638898000199', description: 'Um bom lugar', 
+    address_attributes: { address: 'Rua X', number: '100', city: 'Manaus', state: 'AM', postal_code: '69067-080', neighborhood: 'Centro'})
+
+    inn.inn_rooms.create!(name: 'Quarto com Varanda', size: 35, guest_limit: 3)
+    inn.inn_rooms.create!(name: 'Quarto Térreo', size: 30, guest_limit: 3)
+
+    login_as inn_owner, scope: :inn_owner
+
+    visit root_path
+    click_on 'Gestão de Pousadas'
+    click_on 'Cadastrar Promoção'
+    fill_in 'Nome', with: 'Promoção de Primavera'
+    fill_in 'Data de Início', with: 1.day.from_now
+    fill_in 'Data de Fim', with: 1.week.from_now
+    select 'Quarto Térreo', from: 'Quarto'
+    fill_in 'Valor do desconto (em %)', with: 50
+    click_on 'Criar Promoção'
+
+    within('section#sales') do
+      expect(page).to have_content 'Promoções'
+      expect(page).to have_content 'Promoção de Primavera'
+      expect(page).to have_content "Válida de: #{I18n.l(1.day.from_now.to_date)} á #{I18n.l(1.day.from_now.to_date)}"
+      expect(page).to have_content "Quarto: Quarto Térreo"
+      expect(page).to have_content "Valor do desconto (em %): 50%"
+    end
+  end
 end
