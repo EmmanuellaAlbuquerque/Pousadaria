f1f9d63e7814262a9ea48d149fc80e30aad07110
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-06-03 15:25:49 -0300

    50 min

--- app/controllers/api/v1/inns_controller.rb
+++ app/controllers/api/v1/inns_controller.rb
@@ -4,17 +4,21 @@ class Api::V1::InnsController < Api::V1::ApiController
 
     inn = Inn.where(registration_number: registration_number).first
 
-    render status: 200, 
-           json: inn.as_json(only: 
-                              [
-                                :name,
-                                :registration_number,
-                                :description
-                              ]
-                            )
-                            .merge(
-                              full_address: inn.full_address,
-                              qty_inns: inn.inn_rooms.count
-                            )
+    if inn
+      return render status: 200, 
+             json: inn.as_json(only: 
+                                [
+                                  :name,
+                                  :registration_number,
+                                  :description
+                                ]
+                              )
+                              .merge(
+                                full_address: inn.full_address,
+                                qty_inn_rooms: inn.inn_rooms.count
+                              )
+    else
+      render status: 404, json: { status: "Pousada não encontrada!" }
+    end
   end
 end
--- spec/requests/apis/inns_api_spec.rb
+++ spec/requests/apis/inns_api_spec.rb
@@ -7,19 +7,32 @@ describe 'Inns API' do
       inn_owner = InnOwner.create!(first_name: 'Joao', last_name: 'Almeida', 
                                    document: '53783222001', email: 'joao@email.com', password: '123456')
 
-      inn_owner.create_inn!(name: 'Pousada do Almeidinha', registration_number: '30638898000199', description: 'Um bom lugar', 
+      inn = inn_owner.create_inn!(name: 'Pousada do Almeidinha', registration_number: '30638898000199', description: 'Um bom lugar', 
                             address_attributes: { address: 'Rua X', number: '100', city: 'Manaus', state: 'AM', postal_code: '69067-080', neighborhood: 'Centro'})
 
-      get '/api/v1/inn/search?rn=30638898000199'
+                            
+      inn.inn_rooms.create!(name: 'Quarto com Varanda', size: 35, guest_limit: 3)
+      inn.inn_rooms.create!(name: 'Quarto Térreo', size: 30, guest_limit: 3)
+
+      get '/api/v1/inns/search?rn=30638898000199'
 
       expect(response.status).to eq 200
       expect(response.content_type).to include 'application/json'
       json = JSON.parse(response.body)
       expect(json['name']).to eq 'Pousada do Almeidinha'
       expect(json['registration_number']).to eq '30638898000199'
-      expect(json['description']).to eq '30638898000199'
-      expect(json['full_address']).to eq 'Rua X'
-      expect(json['qty_inn']).to eq 1
+      expect(json['description']).to eq 'Um bom lugar'
+      expect(json['full_address']).to eq 'Rua X, 100 - Centro - Manaus/AM'
+      expect(json['qty_inn_rooms']).to eq 2
+    end
+
+    it 'fails if the Inn is not found' do
+      get '/api/v1/inns/search?rn=30638898000199'
+
+      expect(response.status).to eq 404
+      expect(response.content_type).to include 'application/json'
+      json = JSON.parse(response.body)
+      expect(json['status']).to eq 'Pousada não encontrada!'
     end
   end
 end
